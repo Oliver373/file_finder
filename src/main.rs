c3343e5fbac19c5689658b240990212393196ad8
hanyudong <hanyudong3000@qq.com>
2023-04-01 12:29:46 +0800

    feat: a cli about file finder mvp

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: {} <search_pattern> [start_directory]", args[0]);
+        println!("\nDescription:");
+        println!("  This program searches for files with names containing the specified search pattern.");
+        println!("  If start_directory is not provided, the search starts from the current directory.");
+        return;
+    }
+
+    let search_pattern = &args[1];
+    let start_dir = if args.len() > 2 { &args[2] } else { "." };
+
+    match search_files(start_dir, search_pattern) {
+        Ok(_) => (),
+        Err(e) => eprintln!("Error: {}", e),
+    }
+}
+
+fn search_files<P: AsRef<Path>>(dir: P, search_pattern: &str) -> Result<(), std::io::Error> {
+    let entries = fs::read_dir(dir)?;
+
+    for entry_result in entries {
+        let entry = entry_result?;
+        let path = entry.path();
+        let file_name = path.file_name().unwrap().to_string_lossy();
+
+        if file_name.contains(search_pattern) {
+            println!("{}", path.display());
+        }
+
+        if path.is_dir() {
+            search_files(&path, search_pattern)?;
+        }
+    }
+
+    Ok(())
+}
+
