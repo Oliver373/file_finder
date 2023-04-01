82c4143850f2e684036e4d7bac8179f5661d327b
hanyudong <hanyudong3000@qq.com>
2023-04-01 22:10:48 +0800

    refactor: use thread to improve

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::env;
 use std::fs;
 use std::path::Path;
+use std::sync::mpsc;
+use std::thread;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,36 +14,60 @@ fn main() {
         return;
     }
 
-    let search_pattern = &args[1];
-    let start_dir = if args.len() > 2 { &args[2] } else { "." };
+    let search_pattern = args[1].clone();
+    let start_dir = if args.len() > 2 { args[2].clone() } else { ".".to_string() };
 
-    match search_files(start_dir, search_pattern) {
-        Ok(_) => (),
-        Err(e) => eprintln!("Error: {}", e),
+    let (tx, rx) = mpsc::channel();
+
+    let _ = thread::spawn(move || search_files(&start_dir, &search_pattern, &tx));
+
+    for result in rx {
+        match result {
+            Ok(path) => println!("{}", path),
+            Err(e) => eprintln!("Error: {}", e),
+        }
     }
 }
 
-fn search_files<P: AsRef<Path>>(dir: P, search_pattern: &str) -> Result<(), std::io::Error> {
-    let entries = fs::read_dir(dir)?;
+fn search_files<P: AsRef<Path>>(dir: P, search_pattern: &str,  tx: &mpsc::Sender<Result<String, std::io::Error>>) {
+    let entries = match fs::read_dir(&dir) {
+        Ok(entries) => entries,
+        Err(e) => {
+            let _ = tx.send(Err(e));
+            return;
+        },
+    };
 
     for entry_result in entries {
-        let entry = entry_result?;
+        let entry = match entry_result {
+            Ok(entry) => entry,
+            Err(e) => {
+                let _ = tx.send(Err(e));
+                continue;
+            },
+        };
         let path = entry.path();
         match path.file_name() {
             Some(file_name_os) => {
                 let file_name = file_name_os.to_string_lossy();
                 if file_name.contains(search_pattern) {
-                    println!("{}", path.display());
+                    let _ = tx.send(Ok(path.to_string_lossy().to_string()));
                 }
             },
             None => eprintln!("Failed to get file_name from entry_path: {}", path.display())
         }
 
         if path.is_dir() {
-            search_files(&path, search_pattern)?;
+            let tx_clone = mpsc::Sender::clone(tx);
+            let path_clone = path.clone();
+            let search_pattern_clone = search_pattern.to_owned();
+            let _ = thread::spawn(move || search_files(&path_clone, &search_pattern_clone, &tx_clone));
         }
+        
     }
 
-    Ok(())
+    if dir.as_ref() == Path::new(".") {
+        drop(tx);
+    }
 }
 
