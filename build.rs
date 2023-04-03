3c6becc259903c4ab829fbe5037f4cc0a2b17886
hanyudong <hanyudong3000@qq.com>
2023-04-03 19:09:07 +0800

    doc: fix build doc in english

--- build.rs
+++ build.rs
@@ -2,8 +2,8 @@ use std::env;
 use std::fs;
 use std::path::Path;
 
+/// copy config file to run directory
 fn main() {
-    // 将配置文件复制到工作目录下
     let out_dir = env::var("OUT_DIR").unwrap();
     let target_dir = Path::new(&out_dir).parent().unwrap().parent().unwrap().parent().unwrap();
 
